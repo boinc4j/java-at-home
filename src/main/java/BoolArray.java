a70ab59a5286c8c0d1d2dba034b3a0edd49cb543
Joe Kutner <jpkutner@gmail.com>
2015-12-31 11:28:57 -0600

    better bool array

--- src/main/java/BoolArray.java
+++ src/main/java/BoolArray.java
@@ -5,6 +5,8 @@ import java.io.RandomAccessFile;
 
 public class BoolArray {
 
+  private static final int BYTE_SIZE = Byte.SIZE - 1;
+
   private RandomAccessFile file;
 
   private Long maxSize;
@@ -20,14 +22,41 @@ public class BoolArray {
 
   public Boolean get(Long i) throws IOException {
     if (i >= this.maxSize) throw new ArrayIndexOutOfBoundsException(i.toString());
-    if (i >= file.length()) return false;
-    file.seek(i);
-    return file.readBoolean();
+
+    Long offset = Math.floorDiv(i, BYTE_SIZE);
+    Long index = i % BYTE_SIZE;
+
+    if (offset >= file.length()) return false;
+    file.seek(offset);
+    Byte b = file.readByte();
+
+    return decodeValue(b, index.intValue());
   }
 
   public void set(Long i, Boolean value) throws IOException {
     if (i >= this.maxSize) throw new ArrayIndexOutOfBoundsException(i.toString());
-    file.seek(i);
-    file.writeBoolean(value);
+
+    Long offset = Math.floorDiv(i, BYTE_SIZE);
+    Long index = i % BYTE_SIZE;
+
+    file.seek(offset);
+    Byte oldByte = (offset < file.length()) ? file.readByte() : 0;
+
+    Byte newByte = encodeValue(oldByte, index.intValue(), value);
+    file.writeByte(newByte);
+  }
+
+  public Boolean decodeValue(byte b, int i) {
+    return (b & (0x01 << i)) == (int)(Math.pow(2, i));
+  }
+
+  public Byte encodeValue(Byte b, int i, boolean val) {
+    boolean cur = ((b & (0x01 << i)) != 0);
+    if (val && !cur) {
+      return Integer.valueOf(b.intValue() + (int)(Math.pow(2, i))).byteValue();
+    } else if (!val && cur) {
+      return Integer.valueOf(b.intValue() - (int)(Math.pow(2, i))).byteValue();
+    }
+    return b;
   }
 }
--- src/test/java/BoolArrayTest.java
+++ src/test/java/BoolArrayTest.java
@@ -1,28 +1,37 @@
 import java.io.File;
 import java.io.IOException;
+import java.math.BigInteger;
 import java.nio.file.Files;
 
 public class BoolArrayTest {
 
   private File testFile = new File("target", "bool_array.dat");
 
-  public void setUp() {
-    try {
-      Files.deleteIfExists(testFile.toPath());
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+  public void setUp() throws IOException {
+    Files.deleteIfExists(testFile.toPath());
   }
 
-  public void tearDown() {
-    try {
-      Files.deleteIfExists(testFile.toPath());
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
+  public void tearDown() throws IOException {
+    Files.deleteIfExists(testFile.toPath());
+  }
+
+  public void testInit() throws IOException {
+    setUp();
+    BoolArray a = new BoolArray(testFile);
+
+    assert !a.get(0l);
+    assert !a.get(1l);
+    assert !a.get(2l);
+    assert !a.get(3l);
+    assert !a.get(4l);
+    assert !a.get(5l);
+    assert !a.get(6l);
+    assert !a.get(7l);
+    assert !a.get(8l);
   }
 
   public void testSimple() throws IOException {
+    setUp();
     BoolArray a = new BoolArray(testFile);
 
     a.set(2l, true);
@@ -37,6 +46,7 @@ public class BoolArrayTest {
   }
 
   public void testSize() throws IOException {
+    setUp();
     BoolArray a = new BoolArray(testFile, 7l);
 
     a.set(2l, true);
@@ -52,6 +62,26 @@ public class BoolArrayTest {
     assertIndexOutOfBoundsOnGet(a, 7l);
     assertIndexOutOfBoundsOnGet(a, 8l);
   }
+
+//  public void testMassive() throws IOException {
+//    setUp();
+//
+//    BigInteger ten = BigInteger.valueOf(10l);
+//    BigInteger base = ten.pow(9);
+//    Long largeNumber = base.longValue();
+//    BoolArray a = new BoolArray(testFile, largeNumber+1);
+//
+//    a.set(2l, true);
+//
+//    assert !a.get(0l);
+//    assert a.get(2l);
+//
+//    assert !a.get(largeNumber);
+//
+//    a.set(largeNumber, true);
+//    assert a.get(largeNumber);
+//    assert !a.get(largeNumber-1);
+//  }
 
   private void assertIndexOutOfBoundsOnSet(BoolArray a, Long i, Boolean b) throws IOException {
     try {
