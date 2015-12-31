dc10223089ee5620d0fb2b274d1ca622ca9e547d
Joe Kutner <jpkutner@gmail.com>
2015-12-31 14:14:55 -0600

    clean up

--- src/main/java/IsPrime.java
+++
@@ -1,9 +0,0 @@
-import java.io.PrintWriter;
-
-public class IsPrime {
-  public static void main(String[] args) throws Exception {
-    PrintWriter out = new PrintWriter("out.txt");
-    out.println("It worked!!!");
-    out.close();
-  }
-}
--- src/test/java/FileBackedBoolArrayTest.java
+++ src/test/java/FileBackedBoolArrayTest.java
@@ -1,6 +1,5 @@
 import java.io.File;
 import java.io.IOException;
-import java.math.BigInteger;
 import java.nio.file.Files;
 
 public class FileBackedBoolArrayTest {
