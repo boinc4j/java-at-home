5cccc22483c0f8769d3d9e83175a2752c1e75835
Joe Kutner <jpkutner@gmail.com>
2015-12-31 22:17:29 -0600

    add in file to create work

--- create_work.sh
+++ create_work.sh
@@ -4,6 +4,8 @@ START_SUM=${1}
 CHUNK_SIZE=${2}
 NUM_OF_JOBS=${3}
 
+export PATH=/app/.apt/usr/sbin:/app/.apt/usr/bin:$PATH
+
 cd boinc-project
 
 NEXT_SUM=${START_SUM}
@@ -12,6 +14,7 @@ for JOB in $(seq 0 ${NUM_OF_JOBS}); do
       -wu_name sum_${NEXT_SUM}_${JOB} \
       -wu_template templates/app_in \
       -result_template templates/app_out \
-      -command_line "${NEXT_SUM} ${CHUNK_SIZE}"
+      -command_line "${NEXT_SUM} ${CHUNK_SIZE}" \
+      in.txt
   NEXT_SUM=$((${NEXT_SUM} + ${CHUNK_SIZE}))
 done
